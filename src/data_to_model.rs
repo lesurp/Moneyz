d669f80629d1a6bbefce48362b105805e6868957
Paul Lesur <git@lesurpaul.fr>
2019-08-27 23:07:49 +0200

    THE STUB DATA IS NEEDED
    without it, if the model is empty, the progroam ~crashes~
    https://gitlab.freedesktop.org/cairo/cairo/issues/338

--- src/data_to_model.rs
+++ src/data_to_model.rs
@@ -1,5 +1,5 @@
 use crate::data::*;
-use gtk::{StaticType, GtkListStoreExtManual};
+use gtk::{GtkListStoreExtManual, StaticType};
 
 impl Into<gtk::ListStore> for BudgetCategories {
     fn into(self) -> gtk::ListStore {
@@ -9,6 +9,42 @@ impl Into<gtk::ListStore> for BudgetCategories {
             list.insert_with_values(Some(i), &[0], &[&budget_category.0]);
             i += 1;
         }
+        list.insert_with_values(None, &[0], &[&"ASDA"]);
         list
+    }
+}
+
+impl Into<(gtk::ListStore, gtk::ListStore)> for MonthlyBudget {
+    fn into(self) -> (gtk::ListStore, gtk::ListStore) {
+        let budgets_list = gtk::ListStore::new(&[String::static_type(), String::static_type()]);
+        for (budget_category, budget_amount) in &self.budgets {
+            budgets_list.insert_with_values(
+                None,
+                &[0, 1],
+                &[&budget_category.0, &budget_amount.0.to_string()],
+            );
+        }
+        budgets_list.insert_with_values(None, &[0, 1], &[&"asd", &"123"]);
+
+        let spendings_list = gtk::ListStore::new(&[
+            String::static_type(),
+            String::static_type(),
+            i32::static_type(),
+            i32::static_type(),
+        ]);
+        for spending in self.spendings.0 {
+            spendings_list.insert_with_values(
+                None,
+                &[0, 1, 2, 3],
+                &[
+                    &spending.name,
+                    &spending.budget_category.0,
+                    &spending.amount,
+                    &spending.day.0,
+                ],
+            );
+        }
+        spendings_list.insert_with_values(None, &[0, 1, 2, 3], &[&"asdsadsad", &"35y", &123, &1]);
+        (budgets_list, spendings_list)
     }
 }
