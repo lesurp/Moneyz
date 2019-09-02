612cf3da011ac77d4f57d874311aeb61bddeafc6
Paul Lesur <git@lesurpaul.fr>
2019-09-03 01:04:59 +0200

    cleanup some comments

--- build.rs
+++ build.rs
@@ -1,5 +1,4 @@
 use quote::quote;
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Read, Write};
 use strfmt;
@@ -21,26 +20,26 @@ generate_translation! {
     format_money(whole: i32, cents: i32),
 }
 
-//    format_money(whole: i32, decimal: i32),
 fn main() {
-    let mut locale_to_translation: HashMap<String, TranslationProvider> = HashMap::new();
-
     let mut get_fn = quote! {};
     for entry in std::fs::read_dir("translations").expect("Could not open translation directory") {
         let entry = entry.unwrap();
         let path = entry.path();
+
+        // the joy of OsString -> String
         let filename_no_ext = path.file_stem().unwrap().to_owned().into_string().unwrap();
         let mut file = File::open(&path).unwrap();
         let mut content = String::new();
         file.read_to_string(&mut content)
             .expect("Could not read translation file");
-        // making sure the file is correct - but this step could be skipped
-        let translation: TranslationProvider =
-            serde_json::from_str(&content).expect("Could not deserialize file");
-        // the joy of OsString -> String
-        locale_to_translation.insert(filename_no_ext.clone(), translation);
 
-        // we use the filename as the key to get the TranslationProvider
+        // make sure the file is correct - but this step could technically be skipped
+        let _translation: TranslationProvider =
+            serde_json::from_str(&content).expect("Could not deserialize file");
+
+        // gradually build our getter, using the filename
+        // note: build some Vec<String> language index would also be interesting i.e. to show the
+        // user when selecting a language in a ComboBox
         get_fn = quote! {
                 #get_fn
                 #filename_no_ext => #content,
@@ -48,12 +47,12 @@ fn main() {
     }
 
     get_fn = quote! {
-    pub fn get_provider(locale_id: &str) -> Option<TranslationProvider> {
-        serde_json::from_str(match locale_id {
-                #get_fn
-                _ => return None,
-        }).expect("TranslationProvider construction failed - the build is corrupted!")
-    }
+        pub fn get_provider(locale_id: &str) -> Option<TranslationProvider> {
+            serde_json::from_str(match locale_id {
+                    #get_fn
+                    _ => return None,
+            }).expect("TranslationProvider construction failed - the build is corrupted!")
+        }
     };
 
     let out_dir = std::env::var("OUT_DIR").unwrap();
@@ -61,6 +60,5 @@ fn main() {
     let mut f = File::create(&dest_path).unwrap();
     f.write(TranslationProvider::generated_code().as_bytes())
         .unwrap();
-    f.write("\n".as_bytes()).unwrap();
     f.write(get_fn.to_string().as_bytes()).unwrap();
 }
